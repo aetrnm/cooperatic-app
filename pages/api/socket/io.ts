027baec1b606aab598620664794264c789195f30
Oleh Hnat <oleggnatkovskii@gmail.com>
2023-10-25 14:25:02 +0200

    refactor: rename object

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -1,6 +1,6 @@
 import { Server as NetServer } from "http";
 import { NextApiRequest } from "next";
-import { Server as ServerIO } from "socket.io";
+import { Server } from "socket.io";
 
 import { NextApiResponseServerIo } from "@/types";
 
@@ -14,7 +14,7 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
   if (!res.socket.server.io) {
     const path = "/api/socket/io";
     const httpServer: NetServer = res.socket.server as any;
-    const io = new ServerIO(httpServer, {
+    const io = new Server(httpServer, {
       path: path,
       addTrailingSlash: false,
     });
